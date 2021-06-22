7ef2a4874d6f6e18302885763f401ac660ec4698
Jéssica Suelen <jessiksu@hotmail.com>
2021-06-22 20:01:49 +0100

    Injeção de Dependencia

---
+++ Template.Application/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template.Application.Interfaces
+{
+    public interface IUserService
+    {
+
+        void test();
+    
+    }
+}
